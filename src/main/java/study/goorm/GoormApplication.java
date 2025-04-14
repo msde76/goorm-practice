8a4ce1b50dab8477240fd99ca04e13e3afd7eb7b
. <ngsonging@naver.com>
2025-04-14 22:35:00 +0900

    최초 세팅

---
+++ src/main/java/study/goorm/GoormApplication.java
@@ -0,0 +1,13 @@
+package study.goorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/goorm/GoormApplicationTests.java
@@ -0,0 +1,13 @@
+package study.goorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
